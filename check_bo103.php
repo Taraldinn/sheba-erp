7e8d6cc88a3f32936428a16589c3077c0f8f46f3
taraldinn <kferdoush617@gmail.com>
2026-09-02 00:47:57 +0600

    iniktial commit

--- check_bo103.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'includes/db_config.php';
-$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-$stmt = $pdo->prepare("SELECT status, bill_amount, current_bill_date, bill_position FROM users WHERE user_id = 'bo103'");
-$stmt->execute();
-print_r($stmt->fetch(PDO::FETCH_ASSOC));
